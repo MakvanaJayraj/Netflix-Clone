09310cabb9a244bf8183be811cb54d16e722a558
Makvana Jayraj <makvana.jayraj76537@gmail.com>
2025-07-22 10:28:47 +0530

    Netflix clone done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,20 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css' 
 import cards_data from '../../assets/cards/Cards_data'
-const TitleCards = ({title}) => {
+import { Link } from 'react-router-dom';
+const TitleCards = ({title,category}) => {
+  const [apiData,setApiData]=useState([]);
   const cardRef=useRef();
+
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzIzNTlhODE2ZmQwZWE2MDMyMTAxNTAyYzRlODgyNCIsIm5iZiI6MTc1MzA3ODcyMy45NDQsInN1YiI6IjY4N2RkYmMzZmJjYzJiNjkxNzM4ZDhkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ak7EPLK319ZZXkQWxOjeKL0dmPY3HlHBn4kEu3032cs'
+  }
+};
+
+
 
   const handelWheel=(event)=>{
     event.preventDefault();
@@ -10,19 +22,24 @@ const TitleCards = ({title}) => {
   }
 
   useEffect(()=>{
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:'now_playing'}?language=en-US&page=1`, options)
+  .then(res => res.json())
+  .then(res => setApiData(res.results))
+  .catch(err => console.error(err));
+
     cardRef.current.addEventListener('wheel',handelWheel);
   },[])
-
   return (
     <div className='title-cards'>
       <h2>{title?title:'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardRef}>
       {
-        cards_data.map((card,index)=>{
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        apiData.map((card,index)=>{
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
         })
       }
       </div>
